9866562482bc71d242cef95160d4d92ba2c4d983
Olaf Görlitz <olaf.goerlitz@gmail.com>
2018-12-03 01:36:57 +0100

    Added jupyter startup script and python requirements. Added notebook for preparation of data of german political speeches.

---
+++ start-jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# create virtual environment
+# using venv instead of virtualenv
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install -U -r requirements.txt
+
+jupyter notebook
